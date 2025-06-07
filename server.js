49dc86aa4e913173a61e210edf2bc809ab9257f9
Ângelo Hank <hankangelo6@gmail.com>
2025-06-07 15:39:54 -0300

    criando rota privada e importando pro server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import publicRoutes from './routes/public-routes.js'
-
+import privateRoutes from './routes/private-routes.js'
 const app = express()
 app.use(express.json())
 
 app.use('/', publicRoutes)
+app.use('/', privateRoutes)
 
 app.listen(3000, ()=> {
     console.log("servidor rodando")
